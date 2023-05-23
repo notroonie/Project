4c97864de1cefdbfcfe605fb6ed0d1a67e552937
notroonie <104883242+notroonie@users.noreply.github.com>
2023-05-24 03:36:42 +0530

    Add files via upload

---
+++ CCL_4_5table/main.py
@@ -0,0 +1,19 @@
+import webapp2 
+
+class Mainpage(webapp2.RequestHandler):
+    def get(self):
+
+        for i in range(1,11):
+            self.response.write(5) 
+            self.response.write("X") 
+            self.response.write(i) 
+            self.response.write("=") 
+            self.response.write(5*i)
+            self.response.write("<br>")   
+
+
+
+app = webapp2.WSGIApplication(
+    [("/",Mainpage)], 
+    debug = True
+)
