d17817cee2ff1585ef5ac6c35f6bfdb845ff0ae6
notroonie <104883242+notroonie@users.noreply.github.com>
2023-05-24 03:34:54 +0530

    Add files via upload

---
+++ CCL_1_Helloworld/main.py
@@ -0,0 +1,14 @@
+import webapp2 
+
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Hello World")
+
+        
+
+app = webapp2.WSGIApplication(
+    [("/",MainPage)], 
+    debug = True
+)
